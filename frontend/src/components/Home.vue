8b44f0a6b013ab5890465f328fbd46d3b4a8c954
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2024-12-25 22:00:56 +0530

    Added basic home page that displays content on logged in status

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,9 +1,22 @@
 <script setup>
+import { api } from '@/api';
+import { ref } from 'vue';
+
+async function sendReq(){
+    let res = await api.get("/all");
+    console.log(res)
+    return res.data.payload[0]
+}
+
+const message = ref("")
+sendReq().then(data => {
+    message.value = data;
+})
 
 </script>
 
 <template>
-    <h3>Home!</h3>
+    <h3>Home {{message}} !</h3>
 </template>
 
 <style scoped>
