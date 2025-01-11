bcdf10d4d9f22663b9837312fc1e87d61a7d4d28
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-01-11 10:41:56 +0530

    Frontend: removed some testing code in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,5 @@
 <script setup>
-  import { api } from './api';
   import { RouterView } from 'vue-router';
-
-  async function sendReq(){
-    try{
-      let f = new FormData()
-      f.set("username", "Nykaj")
-      f.set("password", "123456")
-      let res = await api.post("/login",f);
-
-      // let res = await api.post("/logout");
-      return res
-    }
-    catch{
-      return 0
-    }
-  }
-
-  async function access(){
-    try{
-      let res = await api.get("/all");
-      return res.data
-    }
-    catch{
-      return 0
-    }
-  }
 </script>
 
 <template>
