bd0cdf44e42716c9ed1b079992311d0a5929fcfe
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-01-25 20:41:51 +0530

    Frontend changes: Added conditional rendering in lanfding page for admin users

--- frontend/src/pages/admin/AdminDashboard.vue
+++ frontend/src/pages/admin/AdminDashboard.vue
@@ -1,11 +1,23 @@
 <script setup>
 import AdminHeader from '@/components/Header/AdminHeader.vue';
 import { RouterView } from 'vue-router';
+
+import { useRoute } from 'vue-router';
+const route = useRoute();
+console.log(route)
 </script>
 
 <template>
     <AdminHeader/>
-    <RouterView :key="$route.fullPath"/>
+    <template v-if="$route.fullPath !== '/admin' && $route.fullPath !== '/admin/'">
+        <RouterView :key="$route.fullPath"/>
+    </template>
+
+    <template v-else>
+        <div class="d-flex flex-column flex-grow-1 align-items-center mt-3">
+            <h3>Welcome Jakyn!</h3>
+        </div>
+    </template>
 </template>
 
 <style scoped>
