cfd4d5d83bbd82e0ac1e14bc30d5e76abe7fa47a
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-01-30 19:16:45 +0530

    Frontend changes: Modified AllQuestion.vue

--- frontend/src/components/AdminQuestion/AllQuestion.vue
+++ frontend/src/components/AdminQuestion/AllQuestion.vue
@@ -20,14 +20,54 @@ fetchQuestion().then((data)=>{
 <template>
     <div class="d-flex flex-column flex-grow-1 border pt-4">
         <div class="d-flex flex-column align-items-center gap-4">
-            <div v-for="question in questions">
-                <RouterLink :to="`/admin/subjects/${props.sid}/chapters/${props.cid}/questions/${question.id}`">
-                    {{ question.description }}
-                </RouterLink>
+            <div class="question-div" v-for="question in questions">
+                <div class="question-div-layout">
+                    <div class="question-div-decoration">
+                        Q
+                    </div>
+                    <div class="question-div-description">
+                        <RouterLink :to="`/admin/subjects/${props.sid}/chapters/${props.cid}/questions/${question.id}`">
+                            <p class="question-text">{{ question.description.length < 100 ? question.description: question.description.slice(0,100) + "..." }}</p>
+                        </RouterLink>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+.question-div{
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 2em;
+}
+
+.question-div-layout{
+    display: flex;
+    border: 1px solid gold;
+    width: 50em;
+    overflow:clip;
+    gap: 1em;
+}
+
+.question-div-decoration{
+    display: flex;
+    width: 2em;
+    height: 2em;
+    background-color: var(--secondary-color);
+    color: var(--light-color);
+    justify-content: center;
+    align-items: center;
+}
+
+.question-div-description{
+    display: flex;
+}
+
+.question-text{
+    display: flex;
+    align-items: center;
+}
 </style>
