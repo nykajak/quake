56f77eea5391a65e62c176f557dc0c9af8ed8461
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-02-03 05:39:19 +0530

    Frotnend changes: Added dummy buttons for adding and searching to the quiz and some styling

--- frontend/src/pages/admin/quiz/components/AdminQuizQuestions.vue
+++ frontend/src/pages/admin/quiz/components/AdminQuizQuestions.vue
@@ -22,8 +22,12 @@ fetchQuestions().then((data)=>{
     <div class="d-flex flex-column align-items-center">
         <div class="d-flex flex-column align-items-center">
             <h3>Questions</h3>
+            <div class="option-div">
+                <button class="option-button" id="add-button">Add</button>
+                <button class="option-button" id="search-button">Search</button>
+            </div>
 
-            <div class="results-div">
+            <div class="results-div mt-2">
                 <div v-for="question in questions" class="question-div">
                     <div class="d-flex flex-grow-1">
                         <RouterLink :to="`/admin/subjects/${props.sid}/chapters/${props.cid}/questions/${question.id}`">
@@ -43,6 +47,25 @@ fetchQuestions().then((data)=>{
 </template>
 
 <style scoped>
+.option-div{
+    display: flex;
+    flex-direction: row;
+}
+
+.option-button{
+    display: flex;
+    border: none;
+    color: var(--light-color);
+}
+
+#add-button{
+    background-color: var(--tertiary-color);
+}
+
+#search-button{
+    background-color: var(--secondary-color);
+}
+
 .delete-button{
     background-color: var(--error-color);
     color: var(--light-color);
