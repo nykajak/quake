0883e254fb474ffae32ffb0c627e2423abd63cde
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-02-03 05:39:25 +0530

    Frotnend changes: Added dummy buttons for adding and searching to the quiz and some styling

--- frontend/src/pages/admin/quiz/components/AdminQuizQuestions.vue
+++ frontend/src/pages/admin/quiz/components/AdminQuizQuestions.vue
@@ -23,22 +23,19 @@ fetchQuestions().then((data)=>{
         <div class="d-flex flex-column align-items-center">
             <h3>Questions</h3>
             <div class="option-div">
-                <button class="option-button" id="add-button">Add</button>
+                <button class="option-button" id="add-button">Modify</button>
                 <button class="option-button" id="search-button">Search</button>
             </div>
 
             <div class="results-div mt-2">
                 <div v-for="question in questions" class="question-div">
+                    <div class="question-decorator">
+                        Q
+                    </div>
                     <div class="d-flex flex-grow-1">
                         <RouterLink :to="`/admin/subjects/${props.sid}/chapters/${props.cid}/questions/${question.id}`">
-                            {{ question.description.length < 80 ? question.description : question.description.slice(0,75) + "..." }}
+                            {{ question.description.length < 90 ? question.description : question.description.slice(0,85) + "..." }}
                         </RouterLink>
-                    </div>
-    
-                    <div>
-                        <button class="delete-button">
-                            Remove
-                        </button>
                     </div>
                 </div>
             </div>
@@ -66,16 +63,20 @@ fetchQuestions().then((data)=>{
     background-color: var(--secondary-color);
 }
 
-.delete-button{
-    background-color: var(--error-color);
-    color: var(--light-color);
-    border: none
+.question-decorator{
+    display: flex;
+    width: 1.5em;
+    height: 1.5em;
+    justify-content: center;
+    background-color: var(--secondary-color);
+    color: var(--light-color);  
 }
 
 .question-div{
     display: flex;
     width: 40em;
     padding: 0.5em;
+    gap:0.5em;  
 }
 
 .results-div{
