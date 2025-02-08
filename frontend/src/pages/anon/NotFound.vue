3f087411fbb9bed103c77253615e138753c9cde7
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-02-08 14:05:52 +0530

    Frotnend changes: Added styling to NotFound.vue

--- frontend/src/pages/anon/NotFound.vue
+++ frontend/src/pages/anon/NotFound.vue
@@ -1,8 +1,26 @@
 <script setup>
+    import {useThemeStore} from "@/stores/theme.js"
+
+    const themeStore = useThemeStore();
+    document.body.style.colorScheme = themeStore.theme || "light";
+
+    import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    404 Page not found!
+    <div class="d-flex flex-column flex-grow-1 justify-content-center align-items-center">
+        <h4>
+            The page you are looking for does not exist :)
+        </h4>
+
+        <p>
+            Check if URL has been mispelled!
+        </p>
+
+        <RouterLink to="/">
+            Return Home
+        </RouterLink>
+    </div>
 </template>
 
 <style scoped>
