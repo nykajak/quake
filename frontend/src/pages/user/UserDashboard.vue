920725297d0950288a22b258d6f5dc0d65e7437e
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-02-25 07:45:21 +0530

    Frontend changes: Added fetching for username in dashboard

--- frontend/src/pages/user/UserDashboard.vue
+++ frontend/src/pages/user/UserDashboard.vue
@@ -1,19 +1,45 @@
 <script setup>
+
+import { ref } from 'vue';
+import { RouterView } from 'vue-router';
+
 import UserHeader from './components/UserHeader.vue';
-import { RouterView, } from 'vue-router';
+import Loader from '@/components/Loader.vue';
+
+import { api } from '@/api';
+
+const user = ref(null);
+
+async function fetchUser(){
+    try{
+        let res = await api.get("/user/");
+        user.value = res.data.payload;
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+fetchUser();
+
 </script>
 
 <template>
     <UserHeader/>
-    <template v-if="$route.fullPath !== '/user' && $route.fullPath !== '/user/'">
-        <RouterView :key="$route.fullPath"/>
+
+    <template v-if="user">
+        <template v-if="$route.fullPath !== '/user' && $route.fullPath !== '/user/'">
+            <RouterView :key="$route.fullPath"/>
+        </template>
+    
+        <template v-else>
+            <div class="d-flex flex-column flex-grow-1 align-items-center mt-3">
+                <h3>Welcome back {{ user.name }}!</h3>
+            </div>
+        </template>
     </template>
 
-    <template v-else>
-        <div class="d-flex flex-column flex-grow-1 align-items-center mt-3">
-            <h3>Welcome back :)</h3>
-        </div>
-    </template>
+    <Loader v-else/>
 </template>
 
 <style scoped>
