b423adc71bebf0f5c53eec11317cbcb626f02fb8
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-03-01 10:06:35 +0530

    Backend: Docs update for admin/__init__.py

--- backend/api/blueprints/admin/__init__.py
+++ backend/api/blueprints/admin/__init__.py
@@ -1,11 +1,12 @@
 from functools import wraps
-from flask_jwt_extended import jwt_required
 from api.models import *
 from flask import jsonify,Blueprint
 from flask_jwt_extended import get_current_user
 
+# All admin endpoints start with /admin
 admin_routes = Blueprint('admin_routes', __name__, url_prefix="/admin")
 
+# Decorator to ensure that endpoint accessed by admin user only
 def admin_required(fun):
     @wraps(fun)
     def inner(*args,**kwargs):
@@ -15,6 +16,7 @@ def admin_required(fun):
         return jsonify(msg="Not an Admin!"),400
     return inner
 
+# Importing required functionality related to admin responsibilites
 from api.blueprints.admin.users import admin_user_routes
 from api.blueprints.admin.subjects import admin_subject_routes
 from api.blueprints.admin.chapters import admin_chapter_routes
@@ -23,10 +25,23 @@ from api.blueprints.admin.quizes import admin_quiz_routes
 from api.blueprints.admin.enrolled import admin_enrolled_routes
 from api.blueprints.admin.responses import admin_response_routes
 
+# User related endpoints: /admin/users
 admin_routes.register_blueprint(admin_user_routes, url_prefix="/users")
+
+# Subject related endpoints: /admin/subjects
 admin_routes.register_blueprint(admin_subject_routes, url_prefix="/subjects")
+
+# Response related endpoints: /admin/responses
 admin_routes.register_blueprint(admin_response_routes, url_prefix="/responses")
+
+# Enrollment related endpoints: /admin/enrolled
 admin_routes.register_blueprint(admin_enrolled_routes,url_prefix = "/enrolled")
+
+# Chapter related endpoints: /admin/subjects/<sid>/chapters
 admin_subject_routes.register_blueprint(admin_chapter_routes,url_prefix = "/<sid>/chapters")
+
+# Question related endpoints: /admin/subjects/<sid>/chapters/<cid>/questions
 admin_chapter_routes.register_blueprint(admin_question_routes,url_prefix = "/<cid>/questions")
+
+# Quiz related endpoints: /admin/subjects/<sid>/chapters/<cid>/quizes
 admin_chapter_routes.register_blueprint(admin_quiz_routes,url_prefix = "/<cid>/quizes")
