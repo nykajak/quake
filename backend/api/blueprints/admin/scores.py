082e8b3b14a650be4375ed5da3d004f3870133f4
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-03-12 16:48:12 +0530

    Backend: Added rotues to get summary and coverage of quiz attempt

--- backend/api/blueprints/admin/scores.py
+++ backend/api/blueprints/admin/scores.py
@@ -67,3 +67,31 @@ def get_question_coverage_chapter(uid,sid,cid):
     count_attempted = query_attempted.count()
     
     return jsonify(count = count_all, attempted = count_attempted),200
+
+
+@admin_score_routes.get("/users/<uid>/subjects/<sid>/chapters/<cid>/quizes/<qid>")
+@jwt_required()
+@admin_required
+def get_score_summary_quiz(uid,sid,cid,qid):
+    query_all = db.session.query(Response, Quiz, Question).join(Quiz, Quiz.id == Response.quiz_id).join(Question, Question.id == Response.question_id).filter(Quiz.id == qid, Response.user_id == uid)
+    count_all = query_all.count()
+
+    query_correct = query_all.filter(Response.marked == Question.correct)
+    count_correct = query_correct.count()
+    
+    return jsonify(correct = count_correct, count = count_all),200
+
+@admin_score_routes.get("/users/<uid>/subjects/<sid>/chapters/<cid>/quizes/<qid>/coverage")
+@jwt_required()
+@admin_required
+def get_question_coverage_quiz(uid,sid,cid,qid):
+    """
+    Of all questions in quiz how many answered?
+    """
+    query_all = Quiz.query.filter(Quiz.id == qid).scalar().questions
+    count_all = query_all.count()
+
+    query_attempted = db.session.query(Response, Question, Quiz).join(Question, Question.id == Response.question_id).join(Quiz, Quiz.id == Response.quiz_id).filter(Quiz.id == qid, Response.user_id == uid)
+    count_attempted = query_attempted.count()
+    
+    return jsonify(count = count_all, attempted = count_attempted),200
