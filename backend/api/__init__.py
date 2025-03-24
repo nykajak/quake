9dc7ca9ac49c2826848d9cf5fb5107fdf75258c3
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-03-24 18:00:17 +0530

    Docs for setup python script

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,25 +1,35 @@
 import os
+
 from flask import Flask
 from flask_caching import Cache
 from flask_mail import Mail
 from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
-from celery import Celery
+
 from api.database import db
 from api import workers
 
+# Note: Rewrite this file to support multiple config types
+
+# BASE_DIR denotes the /backend/api folder
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+# Flask specific settings
 app = Flask(__name__)
 app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
+
+# FlaskSqlAlchemy settings
 app.config["SQLITE_DB_DIR"] = os.path.join(BASE_DIR, "../instance")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(app.config["SQLITE_DB_DIR"],"quiz.db")
+
+# Flask-jwt-extended settings
 app.config["JWT_TOKEN_LOCATION"] = ["headers", "cookies"]
 app.config["JWT_SECRET_KEY"] = os.getenv("FLASK_SECRET_KEY")
 
-# To be changed: Per instance basis for mail
+# Flask-mail settings
+# Note: Plain-text credentials!
 app.config['MAIL_SERVER'] = 'sandbox.smtp.mailtrap.io'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
@@ -36,10 +46,25 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/2'
 app.config["CACHE_TYPE"] = "SimpleCache"
 app.config["CACHE_DEFAULT_TIMEOUT"] = 300
 
+# Initialising JWT for auth
 jwt = JWTManager(app)
+
+# Initialising bcrypt for hashing
 bcrypt = Bcrypt(app)
+
+# Initialising flask_mail
 mail = Mail(app)
+
+# Initialising flask_cache
 cache = Cache(app)
+
+# Initialising flask_cors for cross site origin requests
+CORS(app,supports_credentials=True)
+
+# Intialising flask_sql_alchemy for db
+db.init_app(app)
+
+# Initialising celery for background jobs
 celery = workers.celery
 celery.conf.update(
     broker_url = app.config['CELERY_BROKER_URL'],
@@ -47,15 +72,11 @@ celery.conf.update(
     timezone = 'Asia/Kolkata',
     enable_utc = False
 )
-
 celery.Task = workers.ContextTask
 
-CORS(app,supports_credentials=True)
-db.init_app(app)
+# Pushing app context
 app.app_context().push()
 
+# Importing all routes and tasks to make them accessible!
 import api.routes
 import api.tasks
-
-# import api.test
-# Rewrite this file to support multiple config types
