05509712fe277f3f07151b35cceff45e10f0d1cd
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-03-25 16:16:59 +0530

    Bug fix, docs, validation in admin/scores

--- backend/api/blueprints/admin/scores.py
+++ backend/api/blueprints/admin/scores.py
@@ -6,11 +6,22 @@ from api.blueprints.admin import admin_required
 # Base URL: /admin/scores
 admin_score_routes = Blueprint('admin_score_routes', __name__)
 
-
 @admin_score_routes.get("/users/<uid>/subjects/<sid>")
 @jwt_required()
 @admin_required
 def get_score_summary_subject(uid,sid):
+    """
+        Return statistics of a particular user for a particular subject.
+        GET /admin/scores/users/:uid/subjects/:sid
+        
+        Expected on success: correct_count, question_count, seen_count, response_count
+        
+        Addional information: correct_count - no of correct responses by this user,
+        response_count - no of responses by this user, question_count - no of questions
+        in this subject, seen_count - no of unique questions attempted by user in subject
+    """
+
+    # Note: Replace query with just Response / Question. No need for querying others!
 
     # Fetch no of questions in subject
     question_count_query = db.session.query(Question, Chapter, Subject)
@@ -40,26 +51,38 @@ def get_score_summary_subject(uid,sid):
 @jwt_required()
 @admin_required
 def get_score_summary_chapter(uid,sid,cid):
-    
-    # Fetch no of questions in subject
+    """
+        Return statistics of a particular user for a particular chapter.
+        GET /admin/scores/users/:uid/subjects/:sid/chapters/:cid
+        
+        Expected on success: correct_count, question_count, seen_count, response_count
+        
+        Addional information: correct_count - no of correct responses by this user,
+        response_count - no of responses by this user, question_count - no of questions
+        in this chapter, seen_count - no of unique questions attempted by user in chapter
+    """
+
+    # Note: Replace query with just Response / Question. No need for querying others!
+
+    # Fetch no of questions in chapter
     question_count_query = db.session.query(Question, Chapter)
     question_count_query = question_count_query.join(Chapter, Chapter.id == Question.chapter_id)
     question_count = question_count_query.count()
 
-    # Fetch no of responses by user in subject
+    # Fetch no of responses by user in chapter
     response_count_query = db.session.query(Response, Question, Chapter)
     response_count_query = response_count_query.join(Question, Question.id == Response.question_id).join(Chapter, Chapter.id == Question.chapter_id)
     response_count_query = response_count_query.filter(Chapter.id == cid,Response.user_id == uid)
     response_count = response_count_query.count()
 
-    # Compute no of questions seen by user in subject
+    # Compute no of questions seen by user in chapter
     questions_seen = set()
     for r,q,c in response_count_query:
         questions_seen.add(q.id)
 
     questions_seen_count = len(questions_seen)
 
-    # Fetch no of correct responses by user in subject
+    # Fetch no of correct responses by user in chapter
     response_correct_query = response_count_query.filter(Response.marked == Question.correct)
     correct_count = response_correct_query.count()
     
@@ -69,7 +92,18 @@ def get_score_summary_chapter(uid,sid,cid):
 @jwt_required()
 @admin_required
 def get_score_summary_quiz(uid,sid,cid,qid):
+    """
+        Return statistics of a particular user for a particular quiz.
+        GET /admin/scores/users/:uid/subjects/:sid/chapters/:cid/quizes/:qid
+        
+        Expected on success: correct_count, question_count, response_count
+        
+        Addional information: correct_count - no of correct responses by this user in 
+        quiz, response_count - no of responses by this user in quiz, question_count - 
+        no of questions in this quiz.
+    """
 
+    # Check for existence of Score object!
     score = Score.query.filter(Score.user_id == uid, Score.quiz_id == qid).scalar()
     if score:
         return jsonify(correct_count = score.correct_count, response_count = score.attempted_count, question_count = score.question_count),200
@@ -77,6 +111,8 @@ def get_score_summary_quiz(uid,sid,cid,qid):
     # Fetch no of questions in a quiz
     question_count_query = Quiz.query.filter(Quiz.id == qid).scalar().questions
     question_count = question_count_query.count()
+
+    # Note: Replace query with just Response / Question. No need for querying others!
 
     # Fetch no of responses for a quiz by some user
     response_count_query = db.session.query(Response, Quiz, Question)
@@ -88,6 +124,7 @@ def get_score_summary_quiz(uid,sid,cid,qid):
     response_correct_query = response_count_query.filter(Response.marked == Question.correct)
     correct_count = response_correct_query.count()
 
+    # Construct a Score object
     score = Score(user_id = uid, quiz_id = qid, attempted_count = response_count, question_count = question_count, correct_count = correct_count)
     db.session.add(score)
     db.session.commit()
