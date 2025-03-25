1b8d7ee2048d443967879321474c812b7698e26e
22f3003212 <22f3003212@ds.study.iitm.ac.in>
2025-03-25 17:21:33 +0530

    Bug fix, docs, validation in user/subjects

--- backend/api/blueprints/user/subjects.py
+++ backend/api/blueprints/user/subjects.py
@@ -4,18 +4,22 @@ from api.models import *
 from api.blueprints.user import user_required
 from api import cache
 
+# Base URL: /user/subjects
 user_subject_routes = Blueprint('user_subject_routes', __name__)
-
 
 @user_subject_routes.get("/all")
 @jwt_required()
 @user_required
 def available_subjects():
     """
+        Returns the list of subjects that user can enroll for
         GET /user/subjects/all
+
+        Expected on success: Returns the serialised, paginated list of subjects
+        that a user can enroll for.
     """
     u = get_current_user()
-    # TO DO - Figure out a way to display all subjects user is allwoed to enroll for efficiently
+    # Note: Figure out a way to display all subjects user is allwoed to enroll for efficiently
 
     enrolled = [x.id for x in u.subjects]
     requested = [x.subject_id for x in db.session.query(Requested).filter(Requested.user_id == u.id)]
@@ -26,6 +30,15 @@ def available_subjects():
 @jwt_required()
 @user_required
 def user_subjects():
+    """
+        Returns the list of subjects that user is enrolled for
+        GET /user/subjects/
+
+        Expected on success: Returns the serialised, paginated list of subjects
+        that a user is enrolled in.
+    """
+
+    # Note: Make this paginated?
     u = get_current_user()
     return jsonify(payload=u.serialise('subjects')),200
 
@@ -33,8 +46,14 @@ def user_subjects():
 @jwt_required()
 @user_required
 def user_specific_subject(sid):
-    # TO DO - Make users be able to access subject metadata (only) for all subjects
+    """
+        Returns specific subject details
+        GET /user/subjects/:sid
 
+        Expected on success: Returns the serialised subject details (plus chapter)
+    """
+    # Note: Should users get to see details of subjects they want to enroll in?
+    # Note: Function to be memoized should not have a mutable object param
     u = get_current_user()
     @cache.memoize(10)
     def fetch_subject(sid,u):
